534e5075196e6f1a9f630d2985d40415cc7cb586
dotori-0 <sesan131@gmail.com>
2022-07-18 18:27:17 +0900

    [Feat] Settings Table View

--- TrendingMedia/SettingsTableViewController.swift
+++ TrendingMedia/SettingsTableViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     
-    let generalSettings = ["공지사항", "실험실", "버전 정보"]
-    let personalSettings = ["개인/보안", "알림", "채팅", "멀티프로필", "집중 모드"]
-    let otherSettings = ["고객센터/도움말"]
-
+    enum Settings: String {
+        case general = "전체 설정"
+        case personal = "개인 설정"
+        case other = "기타"
+        
+        func settingTitles() -> [String] {
+            let generalSettings = ["공지사항", "실험실", "버전 정보"]
+            let personalSettings = ["개인/보안", "알림", "채팅", "멀티프로필", "집중 모드"]
+            let otherSettings = ["고객센터/도움말"]
+            
+            switch self {
+                case .general: return generalSettings
+                case .personal: return personalSettings
+                case .other: return otherSettings
+            }
+        }
+    }
+    
+    //    let generalSettings = ["공지사항", "실험실", "버전 정보"]
+    //    let personalSettings = ["개인/보안", "알림", "채팅", "멀티프로필", "집중 모드"]
+    //    let otherSettings = ["고객센터/도움말"]
+    
+    @IBOutlet weak var concentrationModeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,11 +57,11 @@ class SettingsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
             case 0:
-                return "전체 설정"
+                return Settings.general.rawValue
             case 1:
-                return "개인 설정"
+                return Settings.personal.rawValue
             case 2:
-                return "기타"
+                return Settings.other.rawValue
             default:
                 return "오류"
         }
@@ -49,78 +70,44 @@ class SettingsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
             case 0:
-                return generalSettings.count
+                return Settings.general.settingTitles().count
             case 1:
-                return personalSettings.count
+                return Settings.personal.settingTitles().count
             case 2:
-                return otherSettings.count
+                return Settings.other.settingTitles().count
             default:
                 return 0
         }
     }
     
     
-    
-    
-
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell")!
+        cell.textLabel?.font = .systemFont(ofSize: 13)
+        
+        switch indexPath.section {
+            case 0:
+//                for i in 0..<Settings.general.settingTitles().count {
+                    cell.textLabel?.text = Settings.general.settingTitles()[indexPath.row]
+//                }
+            case 1:
+                cell.textLabel?.text = Settings.personal.settingTitles()[indexPath.row]
+            case 2:
+                cell.textLabel?.text = Settings.other.settingTitles()[indexPath.row]
+            default:
+                print("Error")
+        }
+        
         return cell
     }
-    */
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+    
+
+//    @IBAction func concentrationModeButtonClicked() {
+    @IBAction func concentrationModeButtonClicked(_ sender: UIButton) {        
+        print(#function)
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
