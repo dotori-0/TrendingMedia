f280a83aa9bd437af9ec4f50d09f590dfcad0cee
dotori-0 <sesan131@gmail.com>
2022-07-19 18:53:44 +0900

    [Feat] Set Background Color for TextField Container View

---
+++ TrendingMedia/Extensions/UIView+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Extension.swift
+//  TrendingMedia
+//
+//  Created by SC on 2022/07/19.
+//
+
+import UIKit
+
+extension UIView {
+    func configureTextFieldContainerViewDesign() {
+        self.backgroundColor = UIColor(named: "CellBackgroundColor")
+        self.layer.cornerRadius = 16
+//        self.clipsToBounds = true
+    }
+}
